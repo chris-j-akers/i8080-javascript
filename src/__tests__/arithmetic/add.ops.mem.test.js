771cfbb7bc4643edfc8c5cd083f97be8752d5ede
Chris Akers <chris.j.akers@gmail.com>
2022-05-18 21:21:34 +0100

    Cleaned up ADD Mem test

--- src/__tests__/arithmetic/add.ops.mem.test.js
+++ src/__tests__/arithmetic/add.ops.mem.test.js
@@ -1,19 +1,23 @@
 const Source = require('../../i8080');
 
-// These tests can take a while because they test from each memory location 
-// and there are 2^16 of them (65536). Maybe this is overkill, but I like the idea of
-// testing everything. Can be skipped for most tests I should think.
+// The full test should run to 0xFFFF, but this can take a while. To start off with, and fix
+// any broader bugs, set this to a low value.
+const max_mem_addr = 0x01;
 
 describe('ADD / MEMORY', () => {
+
   test('NO FLAGS SET', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 001 | 0x01 | 00000001 | 000 | 0x00 | 00000000 | 001 | 0x01 | 00000001 |       |
+// +-----------------------+-----------------------+-------+-----------------------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     const data = 0x01;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
@@ -21,28 +25,28 @@ describe('ADD / MEMORY', () => {
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x01);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
-      // Clear
       c.reset();
     }
   });
 
-  test('SET ZERO FLAG', () => {
+  test('SET ZERO', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 000 | 0x01 | 00000001 | 000 | 0x00 | 00000000 | 000 | 0x00 | 00000000 | P|Z   |
+// +-----------------------+-----------------------+-----------------------+-------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     const data = 0x0;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
@@ -50,28 +54,28 @@ describe('ADD / MEMORY', () => {
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x0);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
-      // Clear
       c.reset();
     }
   });
 
-  test('SET PARITY FLAG', () => {
+  test('SET PARITY', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 084 | 0x54 | 01010100 | 001 | 0x01 | 00000001 | 000 | 0x00 | 00000000 | P     |
+// +-----------------------+-----------------------+-----------------------+-------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     const data = 0x54;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
@@ -79,7 +83,6 @@ describe('ADD / MEMORY', () => {
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x55);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
@@ -87,49 +90,51 @@ describe('ADD / MEMORY', () => {
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
-      // Clear
       c.reset();
     }
   });
 
-  test('SET AUX CARRY FLAG', () => {
+  test('SET AUX CARRY', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 015 | 0x0F | 00001111 | 015 | 0x0F | 00001111 | 030 | 0x1E | 00011110 | P|A   |
+// +-----------------------+-----------------------+-----------------------+-------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
-    const data = 0x0f;
+    const data = 0x0F;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
-      c.cpu.accumulator = 0x0f;
+      c.cpu.accumulator = 0x0F;
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x1e);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
-      // Clear
       c.reset();
     }
   });
 
   test('SET SIGN FLAG', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 064 | 0x40 | 01000000 | 112 | 0x70 | 01110000 | 176 | 0xB0 | 10110000 | S     |
+// +-----------------------+-----------------------+-----------------------+-------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     const data = 0x40;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
@@ -137,29 +142,29 @@ describe('ADD / MEMORY', () => {
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0xB0);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
-
-      // Clear
       c.reset();
     }
   });
 
 
   test('SET CARRY FLAG', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 192 | 0xC0 | 11000000 | 066 | 0x42 | 01000010 | 002 | 0x02 | 00000010 | C     |
+// +-----------------------+-----------------------+-----------------------+-------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     const data = 0xC0;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
@@ -167,28 +172,28 @@ describe('ADD / MEMORY', () => {
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x02);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
-      // Clear
       c.reset();
     }
   });
 
   test('UNSET FLAGS', () => {
+// +-----------------------+-----------------------+-----------------------+-------+
+// |       Memory          |      Accumulator      |       Expected        | Flags |
+// +-----------------------+-----------------------+-----------------------+-------+
+// | 001 | 0x01 | 00000001 | 000 | 0x00 | 00000000 | 001 | 0x01 | 00000001 |       |
+// +-----------------------+-----------------------+-------+-----------------------+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     const data = 0x01;
 
-    // Operation (Test all Mem Locations)
-    for (let mem_addr = 0x00; mem_addr <= 0xffff; mem_addr++) {
+    for (let mem_addr = 0x00; mem_addr <= max_mem_addr; mem_addr++) {
       c.bus.write(data, mem_addr);
       c.cpu.scratch_registers.H = (mem_addr >> 8) & 0xff;
       c.cpu.scratch_registers.L = mem_addr & 0xff;
@@ -200,7 +205,6 @@ describe('ADD / MEMORY', () => {
       c.cpu.set_flag(FlagType.Zero);
       c.cpu.set_flag(FlagType.Sign);
 
-      // Check Inputs valid
       expect(c.cpu.flag_set(FlagType.Carry)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
@@ -209,15 +213,12 @@ describe('ADD / MEMORY', () => {
 
       c.cpu.add_mem();
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x01);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
-      // Clear
       c.reset();
     }
   });
