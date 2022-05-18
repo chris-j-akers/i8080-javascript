8f636a0c00a3568d252f57e8d0062ea4bc829dd8
Chris Akers <chris.j.akers@gmail.com>
2022-05-18 16:39:39 +0100

    ADC Accumulator Unit Tests

--- src/__tests__/arithmetic/adc.ops.acc.test.js
+++ src/__tests__/arithmetic/adc.ops.acc.test.js
@@ -1,28 +1,20 @@
 const Source = require('../../i8080');
 
-// Accumulator math is tricky when it comes to setting exact flags. The smallest operation
-// we can perform is to double the number in the accumulator, so we can't run small, 
-// subtle tests like adding 1 (0x1, b00000001) to 15 (0xF, b00001111) to trigger
-// the Aux Carry and *only* the Aux Carry. In a majority of theses tests, more than
-// one flag is set whether the Carry bit is used or not. However, as long as the operation
-// produces the correct result and sets or doesn't set the correct flag, even if it sets 
-// others at the same time, then the test is successful. 
-
 describe('ADC / ACCUMULATOR', () => {
-  test('NO FLAGS SET', () => {
+  test('NO FLAGS SET | CARRY UNSET', () => {
+
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 001 | 0x01 | 00000001 |     0 | 001 | 0x01 | 00000001 |       |
+// +-----------------------+-------+-----------------------+-------+
+    
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     c.cpu.accumulator = 0x02;
-
-    // 1. Without Carry Set
-    c.cpu.clear_flag(FlagType.Carry);
-    
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
     expect(c.cpu.accumulator).toBe(0x04);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
@@ -31,43 +23,23 @@ describe('ADC / ACCUMULATOR', () => {
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
     c.reset();
-
-    // 2. With Carry Set
-
-    // Inputs
-    c.cpu.accumulator = 0x02;
-
-    // 1. Without Carry Set
-    c.cpu.set_flag(FlagType.Carry);
-
-    // Operation
-    c.cpu.adc_reg(c.cpu.accumulator);
-
-    // Results
-    expect(c.cpu.accumulator).toBe(0x05);
-    expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
-    expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
   });
 
-  test('SET ZERO FLAG', () => {
+  test('SET ZERO | CARRY UNSET', () => {
+
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 000 | 0x00 | 00000000 |     0 | 000 | 0x00 | 00000000 | P|Z   |
+// +-----------------------+-------+-----------------------+-------+
 
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     c.cpu.accumulator = 0x00;
-
-    // 1. Without Carry Set - ZERO Flag should be set in this case
     c.cpu.clear_flag(FlagType.Carry);
-    
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
     expect(c.cpu.accumulator).toBe(0x00);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
@@ -75,60 +47,45 @@ describe('ADC / ACCUMULATOR', () => {
     expect(c.cpu.flag_set(FlagType.Zero)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
-    c.reset();
+  });
 
-    // 2. With Carry Set - ZERO Flag should NOT be set
+  test('SET PARITY | CARRY UNSET', () => {
 
-    // Inputs
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 000 | 0x00 | 00000000 |     0 | 000 | 0x00 | 0000000 | P|Z    |
+// +-----------------------+-------+-----------------------+-------+
+  
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
+
     c.cpu.accumulator = 0x00;
-
-    c.cpu.set_flag(FlagType.Carry);
-
-    // Operation
+    c.cpu.clear_flag(FlagType.Carry);
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
-    expect(c.cpu.accumulator).toBe(0x01);
+    expect(c.cpu.accumulator).toBe(0x00);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
+    expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
+    expect(c.cpu.flag_set(FlagType.Zero)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
   });
 
-  test('SET PARITY FLAG', () => {
+  test('SET PARITY | CARRY SET', () => {
+
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 007 | 0x07 | 00000111 |     1 | 015 | 0x0F | 00001111 | P     |
+// +-----------------------+-------+-----------------------+-------+
+      
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     c.cpu.accumulator = 0x07;
-
-    // 1. Without Carry Set
-    c.cpu.clear_flag(FlagType.Carry);
-  
-    // Operation
-    c.cpu.adc_reg(c.cpu.accumulator);
-
-
-    // Results
-    expect(c.cpu.accumulator).toBe(0x0E);
-    expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-
-    c.reset();
-
-    // 2. With Carry Set
-
-    // Inputs
-    c.cpu.accumulator = 0x07;
-
     c.cpu.set_flag(FlagType.Carry);
-
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
     // Results
@@ -141,44 +98,44 @@ describe('ADC / ACCUMULATOR', () => {
 
   });
 
-  test('SET AUX CARRY FLAG', () => {
+  test('SET AUX CARRY | CARRY UNSET', () => {
 
-    // Weird one to test with an accumulator because it's adding to itself so can only
-    // double. This means we can't try an AuxCarry NOT set without the Carry flag, first.
-    //
-    // Basically, both tests should set the AuxCarry flag, but, we do check the results
-    // are correct which gives some confidence the test works.
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 009 | 0x09 | 00001001 |     0 | 018 | 0x12 | 00010010 | P|A   |
+// +-----------------------+-------+-----------------------+-------+
 
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
-    c.cpu.accumulator = 0x08;
-
-    // Operation
+    c.cpu.accumulator = 0x09;
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
-    expect(c.cpu.accumulator).toBe(0x10);
+    expect(c.cpu.accumulator).toBe(0x12);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
+    expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
-    c.reset();
+  });
 
-    // 2. With Carry Set
+  test('SET AUX CARRY | CARRY SET', () => {
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 008 | 0x08 | 00001000 |     1 | 017 | 0x11 | 00010001 | P|A   |
+// +-----------------------+-------+-----------------------+-------+
+    
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     c.cpu.accumulator = 0x08;
 
     c.cpu.set_flag(FlagType.Carry);
-
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
     expect(c.cpu.accumulator).toBe(0x11);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
@@ -186,46 +143,45 @@ describe('ADC / ACCUMULATOR', () => {
     expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
-
   });
 
-  test('SET SIGN FLAG', () => {
-    // Similar to above (Aux Carry) it's adding to itself so can only
-    // double. This means we can't try a SIGN NOT set without the Carry flag, first.
-    //
-    // Basically, both tests should set the SIGN flag, but, we do check the results
-    // are correct which gives some confidence the test works.
+  test('SET SIGN | CARRY UNSET', () => {
+    // +-----------------------+-------+-----------------------+-------+
+    // |      Accumulator      | Carry |      Expected         | Flags |
+    // +-----------------------+-------+-----------------------+-------+
+    // | 089 | 0x59 | 01011001 |     0 | 177 | 0xB1 | 10110001 | P|A|S |
+    // +-----------------------+-------+-----------------------+-------+
 
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
-    c.cpu.accumulator = 0x58;
-
-    // Operation
+    c.cpu.accumulator = 0x59;
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
-    expect(c.cpu.accumulator).toBe(0xb0);
+    expect(c.cpu.accumulator).toBe(0xB2);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-    expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
+    expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
+  });
 
-    c.reset();
 
-    // 2. With Carry Set
+  test('SET SIGN | CARRY SET', () => {
 
-    // Inputs
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 088 | 0x58 | 01011000 |     1 | 177 | 0xB1 | 10110001 | P|A|S |
+// +-----------------------+-------+-----------------------+-------+
+
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
+
     c.cpu.accumulator = 0x58;
-
     c.cpu.set_flag(FlagType.Carry);
-
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
     expect(c.cpu.accumulator).toBe(0xb1);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
@@ -236,44 +192,45 @@ describe('ADC / ACCUMULATOR', () => {
 
   });
 
-  test('SET CARRY FLAG', () => {
-    // Similar to above (Aux Carry) it's adding to itself so can only
-    // double. This means we can't try a CARRY NOT set without the Carry flag, first.
-    //
-    // Basically, both tests should set the SIGN flag, but, we do check the results
-    // are correct which gives some confidence the test works.
+  test('SET CARRY | CARRY UNSET', () => {
+
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 241 | 0xF1 | 11110001 |     0 | 226 | 0xE2 | 11100010 | C|P|S |
+// +-----------------------+-------+-----------------------+-------+
 
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
-    c.cpu.accumulator = 0xf0;
-
-    // Operation
+    c.cpu.accumulator = 0xF1;
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
-    expect(c.cpu.accumulator).toBe(0xe0);
+    expect(c.cpu.accumulator).toBe(0xE2);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeTruthy();
-    expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
+    expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
 
-    c.reset();
+  });
 
-    // 2. With Carry Set
+test('SET CARRY | CARRY SET', () => {
 
-    // Inputs
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 240 | 0xF0 | 11110000 |     1 | 225 | 0xE1 | 11100001 | C|P|S |
+// +-----------------------+-------+-----------------------+-------+
+  
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
+
     c.cpu.accumulator = 0xf0;
-
     c.cpu.set_flag(FlagType.Carry);
-
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
-    expect(c.cpu.accumulator).toBe(0xe1);
+    expect(c.cpu.accumulator).toBe(0xE1);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
@@ -281,31 +238,34 @@ describe('ADC / ACCUMULATOR', () => {
     expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
   });
 
-  test('UNSET FLAGS', () => {
+  test('UNSET FLAGS | CARRY UNSET', () => {
+
+  // +-----------------------+-------+-----------------------+-------+
+  // |      Accumulator      | Carry |      Expected         | Flags |
+  // +-----------------------+-------+-----------------------+-------+
+  // | 001 | 0X01 | 00000001 |     0 | 002 | 0x02 | 00000010 |       |
+  // +-----------------------+-------+-----------------------+-------+
 
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // Inputs
     c.cpu.accumulator = 0x01;
 
-    // Set all flags EXCEPT Carry
     c.cpu.set_flag(FlagType.Parity);
     c.cpu.set_flag(FlagType.AuxillaryCarry);
     c.cpu.set_flag(FlagType.Zero);
     c.cpu.set_flag(FlagType.Sign);
 
-    // Check Inputs valid
+    // We're not setting this as this is the Carry test, remember!
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
+
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Zero)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
 
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
     expect(c.cpu.accumulator).toBe(0x02);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
@@ -313,37 +273,44 @@ describe('ADC / ACCUMULATOR', () => {
     expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
-    c.reset();
+  });
 
-    // 2. With Carry Set
+  test('UNSET FLAGS | CARRY SET', () => {
 
-    // Inputs
+// +-----------------------+-------+-----------------------+-------+
+// |      Accumulator      | Carry |      Expected         | Flags |
+// +-----------------------+-------+-----------------------+-------+
+// | 001 | 0X01 | 00000001 |     1 | 003| 0x03 | 00000011  | P     |
+// +-----------------------+-------+-----------------------+-------+
+    
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
+
     c.cpu.accumulator = 0x01;
 
-    // Set all flags INCLUDING Carry
     c.cpu.set_flag(FlagType.Carry);
     c.cpu.set_flag(FlagType.Parity);
     c.cpu.set_flag(FlagType.AuxillaryCarry);
     c.cpu.set_flag(FlagType.Zero);
     c.cpu.set_flag(FlagType.Sign);
 
-    // Check Inputs valid
     expect(c.cpu.flag_set(FlagType.Carry)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Zero)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
 
-    // Operation
     c.cpu.adc_reg(c.cpu.accumulator);
 
-    // Results
     expect(c.cpu.accumulator).toBe(0x03);
     expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
+    // Can't stop Parity being truthy, here, because we'll always end up with and odd
+    // number in the Accumulator tests with a Carry, so always an even number of bits.
     expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
     expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
     expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
   });
+
 });
