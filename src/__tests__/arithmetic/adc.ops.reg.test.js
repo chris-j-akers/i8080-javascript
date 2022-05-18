a12b7dc138f4ec583abc9adb2735a359aa1d6851
Chris Akers <chris.j.akers@gmail.com>
2022-05-18 16:51:12 +0100

    ADC Register Unit Tests

--- src/__tests__/arithmetic/adc.ops.reg.test.js
+++ src/__tests__/arithmetic/adc.ops.reg.test.js
@@ -2,7 +2,7 @@ const Source = require('../../i8080');
 
 describe('ADC / REGISTERS', () => {
 
-  test('NO FLAGS SET | NO CARRY', () => {
+  test('NO FLAGS SET | CARRY UNSET', () => {
 
 // +-----------------------+-----------------------+-------+-----------------------+-------+
 // |       Register        |      Accumulator      | Carry |       Expected        | Flags |
@@ -29,7 +29,7 @@ describe('ADC / REGISTERS', () => {
     }
   });
 
-  test('NO FLAGS SET | CARRY', () => {
+  test('NO FLAGS SET | CARRY SET', () => {
 
 // +-----------------------+-----------------------+-------+-----------------------+-------+
 // |       Register        |      Accumulator      | Carry |       Expected        | Flags |
@@ -57,7 +57,7 @@ describe('ADC / REGISTERS', () => {
     }
   });
 
-  test('SET ZERO FLAG | NO CARRY', () => {
+  test('SET ZERO | CARRY UNSET', () => {
 
 // +-----------------------+-----------------------+-------+-----------------------+-------+
 // |       Register        |      Accumulator      | Carry |       Expected        | Flags |
@@ -84,40 +84,13 @@ describe('ADC / REGISTERS', () => {
     }
   });
 
-  test('SET ZERO FLAG | CARRY', () => {
+
+  test('SET PARITY | CARRY UNSET)', () => {
 
 // +-----------------------+-----------------------+-------+-----------------------+-------+
 // |       Register        |      Accumulator      | Carry |       Expected        | Flags |
 // +-----------------------+-----------------------+-------+-----------------------+-------+
-// | 000 | 0x00 | 00000000 | 000 | 0x00 | 00000000 |     1 | 001 | 0x01 | 00000001 |       |
-// +-----------------------+-----------------------+-------+-----------------------+-------+
-
-    const c = new Source.Computer();
-    const FlagType = Source.i8080.FlagType;
-
-    for (reg in c.cpu.scratch_registers) {
-      c.cpu.accumulator = 0x00;
-      c.cpu.scratch_registers[reg] = 0x00;
-
-      c.cpu.set_flag(FlagType.Carry);
-      c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
-
-      expect(c.cpu.accumulator).toBe(0x01);
-      expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-      c.reset();
-    }
-  });
-
-  test('SET PARITY FLAG | NO CARRY)', () => {
-
-// +-----------------------+-----------------------+-------+-----------------------+-------+
-// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
-// +-----------------------+-----------------------+-------+-----------------------+-------+
-// | 083 | 0x53 | 01010011 | 001 | 0x01 | 00000001 |     0 | 084 | 0x54 | 01010100 |       |
+// | 084 | 0x54 | 00110110 | 001 | 0x01 | 00000001 |     0 | 085 | 0x55 | 00110111 | P     |
 // +-----------------------+-----------------------+-------+-----------------------+-------+
 
     const c = new Source.Computer();
@@ -125,15 +98,13 @@ describe('ADC / REGISTERS', () => {
 
     for (reg in c.cpu.scratch_registers) {
       c.cpu.accumulator = 0x01;
-      c.cpu.scratch_registers[reg] = 0x53;
+      c.cpu.scratch_registers[reg] = 0x54;
 
-      // Operation
       c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
 
-      // Results
-      expect(c.cpu.accumulator).toBe(0x54);
+      expect(c.cpu.accumulator).toBe(0x55);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
+      expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
@@ -141,7 +112,7 @@ describe('ADC / REGISTERS', () => {
     }
   });
 
-  test('SET PARITY FLAG | CARRY)', () => {
+  test('SET PARITY | CARRY SET)', () => {
 
 // +-----------------------+-----------------------+-------+-----------------------+-------+
 // |       Register        |      Accumulator      | Carry |       Expected        | Flags |
@@ -170,39 +141,23 @@ describe('ADC / REGISTERS', () => {
 
   });
 
-  test('SET AUX CARRY FLAG', () => {
+  test('SET AUX CARRY | CARRY UNSET', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 015 | 0x0F | 00001111 | 001 | 0x01 | 00000001 |     0 | 016 | 0x10 | 00010000 | A     |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
     for (reg in c.cpu.scratch_registers) {
       c.cpu.accumulator = 0x01;
-      c.cpu.scratch_registers[reg] = 0xE;
+      c.cpu.scratch_registers[reg] = 0x0F;
 
-      // Operation
       c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
 
-      // Results
-      expect(c.cpu.accumulator).toBe(0xF);
-      expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
-      expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
-      c.reset();
-    }
-
-    // 2. With Carry
-    c.reset();
-
-    for (reg in c.cpu.scratch_registers) {
-      c.cpu.accumulator = 0x01;
-      c.cpu.scratch_registers[reg] = 0xE;
-      c.cpu.set_flag(FlagType.Carry);
-
-      // Operation
-      c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
-
-      // Results
       expect(c.cpu.accumulator).toBe(0x10);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
@@ -213,39 +168,51 @@ describe('ADC / REGISTERS', () => {
     }
   });
 
-  test('SET SIGN FLAG', () => {
+  test('SET AUX CARRY | CARRY SET', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 014 | 0x0E | 01010011 | 001 | 0x01 | 00000001 |     1 | 016 | 0x10 | 00010000 | A     |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // 1. Without Carry
-
-    // Inputs (let's do all registers!)
     for (reg in c.cpu.scratch_registers) {
-      c.cpu.accumulator = 0x1;
-      c.cpu.scratch_registers[reg] = 0x7E;
+      c.cpu.accumulator = 0x01;
+      c.cpu.scratch_registers[reg] = 0x0E;
+      c.cpu.set_flag(FlagType.Carry);
 
-      // Operation
       c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
 
-      // Results
-      expect(c.cpu.accumulator).toBe(0x7f);
+      expect(c.cpu.accumulator).toBe(0x10);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
-      expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeFalsy();
+      expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
       expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
       c.reset();
     }
+  });
+
+  test('SET SIGN | CARRY UNSET', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 127 | 0x7F | 01111111 | 001 | 0x01 | 00000001 |     0 | 128 | 0x80 | 10000000 | A|S   |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
 
     for (reg in c.cpu.scratch_registers) {
-      c.cpu.accumulator = 0x1;
-      c.cpu.scratch_registers[reg] = 0x7e;
-      c.cpu.set_flag(FlagType.Carry);
+      c.cpu.accumulator = 0x01;
+      c.cpu.scratch_registers[reg] = 0x7F;
 
-      // Operation
       c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
 
-      // Results
       expect(c.cpu.accumulator).toBe(0x80);
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
@@ -254,23 +221,53 @@ describe('ADC / REGISTERS', () => {
       expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
       c.reset();
     }
-
   });
 
-  test('SET CARRY FLAG', () => {
+  test('SET SIGN | CARRY SET', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 126 | 0x7E | 01111110 | 001 | 0x01 | 00000001 |     1 | 128 | 0x80 | 10000000 | S|A |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+    
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
-    // 1. Without Carry flag
-    // Inputs (let's do all registers!)
     for (reg in c.cpu.scratch_registers) {
-      c.cpu.accumulator = 0x1;
-      c.cpu.scratch_registers[reg] = 0xfe;
+      c.cpu.accumulator = 0x01;
+      c.cpu.scratch_registers[reg] = 0x7E;
+      c.cpu.set_flag(FlagType.Carry);
 
-      // Operation
+      c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
+
+      expect(c.cpu.accumulator).toBe(0x80);
+      expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
+      expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
+      expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
+      expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
+      expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
+      c.reset();
+    }
+  });
+
+  test('SET CARRY | CARRY UNSET', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 255 | 0xFF | 11111111 | 001 | 0x01 | 00000001 |     0 | 000 | 0x00 | 00000000 | C|P|A |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+    
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
+
+    for (reg in c.cpu.scratch_registers) {
+      c.cpu.accumulator = 0x01;
+      c.cpu.scratch_registers[reg] = 0xFE;
+
       c.cpu.add_reg(c.cpu.scratch_registers[reg]);
 
-      // Results
       expect(c.cpu.accumulator).toBe(0xff); 
       expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
       expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
@@ -279,10 +276,21 @@ describe('ADC / REGISTERS', () => {
       expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
       c.reset();
     }
+  });
 
+  test('SET CARRY | CARRY SET', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 254 | 0xFE | 11111110 | 001 | 0x01 | 00000001 |     1 | 000 | 0x00 | 00000000 | C|P|A |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+    
+    const c = new Source.Computer();
+    const FlagType = Source.i8080.FlagType;
     for (reg in c.cpu.scratch_registers) {
-      c.cpu.accumulator = 0x1;
-      c.cpu.scratch_registers[reg] = 0xfe;
+      c.cpu.accumulator = 0x01;
+      c.cpu.scratch_registers[reg] = 0xFE;
       c.cpu.set_flag(FlagType.Carry);
 
       // Operation
@@ -300,6 +308,13 @@ describe('ADC / REGISTERS', () => {
   });
 
   test('UNSET FLAGS', () => {
+
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// |       Register        |      Accumulator      | Carry |       Expected        | Flags |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+// | 000 | 0x00 | 00000000 | 001 | 0x01 | 00000001 |     1 | 002 | 0x02 | 00000010 |       |
+// +-----------------------+-----------------------+-------+-----------------------+-------+
+
     const c = new Source.Computer();
     const FlagType = Source.i8080.FlagType;
 
@@ -314,17 +329,14 @@ describe('ADC / REGISTERS', () => {
         c.cpu.set_flag(FlagType.Zero);
         c.cpu.set_flag(FlagType.Sign);
 
-        // Check Inputs valid
         expect(c.cpu.flag_set(FlagType.Carry)).toBeTruthy();
         expect(c.cpu.flag_set(FlagType.Parity)).toBeTruthy();
         expect(c.cpu.flag_set(FlagType.AuxillaryCarry)).toBeTruthy();
         expect(c.cpu.flag_set(FlagType.Zero)).toBeTruthy();
         expect(c.cpu.flag_set(FlagType.Sign)).toBeTruthy();
 
-        // Operation
         c.cpu.adc_reg(c.cpu.scratch_registers[reg]);
 
-        // Results
         expect(c.cpu.accumulator).toBe(0x02);
         expect(c.cpu.flag_set(FlagType.Carry)).toBeFalsy();
         expect(c.cpu.flag_set(FlagType.Parity)).toBeFalsy();
@@ -332,10 +344,7 @@ describe('ADC / REGISTERS', () => {
         expect(c.cpu.flag_set(FlagType.Zero)).toBeFalsy();
         expect(c.cpu.flag_set(FlagType.Sign)).toBeFalsy();
 
-        // Clear
         c.reset();
     }
-
-
   });
 });
