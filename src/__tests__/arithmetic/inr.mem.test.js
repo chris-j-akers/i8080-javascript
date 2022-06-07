0a71de9851d806dc1f378cf3b9b518fab719eb5e
Chris Akers <chris.j.akers@gmail.com>
2022-06-07 14:03:03 +0100

    INR  Tests added

---
+++ src/__tests__/arithmetic/inr.mem.test.js
@@ -0,0 +1,198 @@
+import { Computer } from '../../computer.js'
+import { i8080 } from '../../i8080.js'
+import { strict as assert } from 'assert'
+
+describe('INR Memory', () => {
+	it('Set no flags', () => {
+		const max_mem_addr = 255;
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		const data = 0;
+		
+		const program = [
+		  0x26,                   // MVI into H...
+		  null,                   // ...the high-byte of the memory address (ro be inserted)
+		  0x2E,                   // MVI into L...
+		  null,                   // ... the low-byte of the memory address (to be inserted)
+		  0x36,                   // MVI into the memory address...
+		  data,                   // ...the data value
+		  0x34,                   // INR data in Memory Location
+		  0x76                    // Halt Program
+		]
+		
+		/**
+		  * Our little test program already takes up some memory,
+		  * so we start tests after the code.
+		  */
+		
+		let carry_set;
+		for (let mem_addr = program.length; mem_addr <= max_mem_addr; mem_addr++) {
+		  program[1] = (mem_addr >> 8) & 0xFF;
+		  program[3] = mem_addr & 0xFF;
+		
+		  carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		  c.inject_program(program);
+		  c.execute_program();
+		
+		  // Here, we're just checking the carry flag hasn't been touched.
+		  assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		  assert.equal(c.bus.read(mem_addr),1);
+		  assert.equal(c.cpu.flag_set(FlagType.Parity), false);
+		  assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), false);
+		  assert.equal(c.cpu.flag_set(FlagType.Zero), false);
+		  assert.equal(c.cpu.flag_set(FlagType.Sign), false);
+		  
+		  assert.equal(c.cpu.clock, 41);
+		  
+		  c.reset();
+		  }
+		});
+		
+	it('Rollover from 255 (Set Parity, AC and Zero flags)', () => {
+		const max_mem_addr = 255;
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		const data = 255;
+		
+		const program = [
+		  0x26,                   // MVI into H...
+		  null,                   // ...the high-byte of the memory address (ro be inserted)
+		  0x2E,                   // MVI into L...
+		  null,                   // ... the low-byte of the memory address (to be inserted)
+		  0x36,                   // MVI into the memory address...
+		  data,                   // ...the data value
+		  0x34,                   // INR data in Memory Location
+		  0x76                    // Halt Program
+		]
+		
+		/**
+		  * Our little test program already takes up some memory,
+		  * so we start tests after the code.
+		  */
+		
+		let carry_set;
+		for (let mem_addr = program.length; mem_addr <= max_mem_addr; mem_addr++) {
+		  program[1] = (mem_addr >> 8) & 0xFF;
+		  program[3] = mem_addr & 0xFF;
+		
+		  carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		  c.inject_program(program);
+		  c.execute_program();
+		
+		  // Here, we're just checking the carry flag hasn't been touched.
+		  assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		  assert.equal(c.bus.read(mem_addr),0);
+		  assert.equal(c.cpu.flag_set(FlagType.Parity), true);
+		  assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), true);
+		  assert.equal(c.cpu.flag_set(FlagType.Zero), true);
+		  assert.equal(c.cpu.flag_set(FlagType.Sign), false);
+		  
+		  assert.equal(c.cpu.clock, 41);
+		  
+		  c.reset();
+		  }
+		});
+		
+	it('Set Parity Flag', () => {
+		const max_mem_addr = 255;
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		const data = 84;
+		
+		const program = [
+		  0x26,                   // MVI into H...
+		  null,                   // ...the high-byte of the memory address (ro be inserted)
+		  0x2E,                   // MVI into L...
+		  null,                   // ... the low-byte of the memory address (to be inserted)
+		  0x36,                   // MVI into the memory address...
+		  data,                   // ...the data value
+		  0x34,                   // INR data in Memory Location
+		  0x76                    // Halt Program
+		]
+		
+		/**
+		  * Our little test program already takes up some memory,
+		  * so we start tests after the code.
+		  */
+		
+		let carry_set;
+		for (let mem_addr = program.length; mem_addr <= max_mem_addr; mem_addr++) {
+		  program[1] = (mem_addr >> 8) & 0xFF;
+		  program[3] = mem_addr & 0xFF;
+		
+		  carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		  c.inject_program(program);
+		  c.execute_program();
+		
+		  // Here, we're just checking the carry flag hasn't been touched.
+		  assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		  assert.equal(c.bus.read(mem_addr),85);
+		  assert.equal(c.cpu.flag_set(FlagType.Parity), true);
+		  assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), false);
+		  assert.equal(c.cpu.flag_set(FlagType.Zero), false);
+		  assert.equal(c.cpu.flag_set(FlagType.Sign), false);
+		  
+		  assert.equal(c.cpu.clock, 41);
+		  
+		  c.reset();
+		  }
+		});
+		
+	it('Set Sign Flag', () => {
+		const max_mem_addr = 255;
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		const data = 175;
+		
+		const program = [
+		  0x26,                   // MVI into H...
+		  null,                   // ...the high-byte of the memory address (ro be inserted)
+		  0x2E,                   // MVI into L...
+		  null,                   // ... the low-byte of the memory address (to be inserted)
+		  0x36,                   // MVI into the memory address...
+		  data,                   // ...the data value
+		  0x34,                   // INR data in Memory Location
+		  0x76                    // Halt Program
+		]
+		
+		/**
+		  * Our little test program already takes up some memory,
+		  * so we start tests after the code.
+		  */
+		
+		let carry_set;
+		for (let mem_addr = program.length; mem_addr <= max_mem_addr; mem_addr++) {
+		  program[1] = (mem_addr >> 8) & 0xFF;
+		  program[3] = mem_addr & 0xFF;
+		
+		  carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		  c.inject_program(program);
+		  c.execute_program();
+		
+		  // Here, we're just checking the carry flag hasn't been touched.
+		  assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		  assert.equal(c.bus.read(mem_addr),176);
+		  assert.equal(c.cpu.flag_set(FlagType.Parity), false);
+		  assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), true);
+		  assert.equal(c.cpu.flag_set(FlagType.Zero), false);
+		  assert.equal(c.cpu.flag_set(FlagType.Sign), true);
+		  
+		  assert.equal(c.cpu.clock, 41);
+		  
+		  c.reset();
+		  }
+		});
+		
+});
---
+++ src/__tests__/arithmetic/inr.reg.test.js
@@ -0,0 +1,165 @@
+import { Computer } from '../../computer.js'
+import { i8080 } from '../../i8080.js'
+import { strict as assert } from 'assert'
+
+/**
+* An opcode lookup table allows us to programmatically determine which OpCodes
+* to use, depending on the register being tested. It saves us having to write a
+* separate test per register. Instead we can loop through each one and run the
+* same test.
+*/
+const opcode_lookup = {
+    'B': {MVI: 0x06, INR: 0x04},
+    'C': {MVI: 0x0E, INR: 0x0C},
+    'D': {MVI: 0x16, INR: 0x14},
+    'E': {MVI: 0x1E, INR: 0x1C},
+    'H': {MVI: 0x26, INR: 0x24},
+    'L': {MVI: 0x2E, INR: 0x2C}
+};
+
+describe('INR R', () => {
+	it('Set no flags', () => {
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		let program = [
+		    null,
+		    0,
+		    null,
+		    0x76            // HALT
+		]
+		
+		let carry_set;
+		for (let reg of Object.keys(c.cpu.registers).filter((register) => register != 'A')) {
+		    program[0] = opcode_lookup[reg].MVI;
+		    program[2] = opcode_lookup[reg].INR;
+		
+		    carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		    c.inject_program(program);
+		    c.execute_program();
+		
+		    // Here, we're just checking the carry flag hasn't been touched.
+		    assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		    assert.equal(c.cpu.registers[reg], 1);
+		    assert.equal(c.cpu.flag_set(FlagType.Parity),false);
+		    assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), false);
+		    assert.equal(c.cpu.flag_set(FlagType.Zero), false);
+		    assert.equal(c.cpu.flag_set(FlagType.Sign), false);
+		
+		    assert.equal(c.cpu.clock, 19);
+		    
+		    c.reset();
+		};
+		});
+		
+	it('Rollover from 255 (Set Parity, AC and Zero flags)', () => {
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		let program = [
+		    null,
+		    255,
+		    null,
+		    0x76            // HALT
+		]
+		
+		let carry_set;
+		for (let reg of Object.keys(c.cpu.registers).filter((register) => register != 'A')) {
+		    program[0] = opcode_lookup[reg].MVI;
+		    program[2] = opcode_lookup[reg].INR;
+		
+		    carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		    c.inject_program(program);
+		    c.execute_program();
+		
+		    // Here, we're just checking the carry flag hasn't been touched.
+		    assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		    assert.equal(c.cpu.registers[reg], 0);
+		    assert.equal(c.cpu.flag_set(FlagType.Parity),true);
+		    assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), true);
+		    assert.equal(c.cpu.flag_set(FlagType.Zero), true);
+		    assert.equal(c.cpu.flag_set(FlagType.Sign), false);
+		
+		    assert.equal(c.cpu.clock, 19);
+		    
+		    c.reset();
+		};
+		});
+		
+	it('Set Parity Flag', () => {
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		let program = [
+		    null,
+		    84,
+		    null,
+		    0x76            // HALT
+		]
+		
+		let carry_set;
+		for (let reg of Object.keys(c.cpu.registers).filter((register) => register != 'A')) {
+		    program[0] = opcode_lookup[reg].MVI;
+		    program[2] = opcode_lookup[reg].INR;
+		
+		    carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		    c.inject_program(program);
+		    c.execute_program();
+		
+		    // Here, we're just checking the carry flag hasn't been touched.
+		    assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		    assert.equal(c.cpu.registers[reg], 85);
+		    assert.equal(c.cpu.flag_set(FlagType.Parity),true);
+		    assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), false);
+		    assert.equal(c.cpu.flag_set(FlagType.Zero), false);
+		    assert.equal(c.cpu.flag_set(FlagType.Sign), false);
+		
+		    assert.equal(c.cpu.clock, 19);
+		    
+		    c.reset();
+		};
+		});
+		
+	it('Set Sign Flag', () => {
+		const c = new Computer();
+		const FlagType = i8080.FlagType;
+		
+		let program = [
+		    null,
+		    175,
+		    null,
+		    0x76            // HALT
+		]
+		
+		let carry_set;
+		for (let reg of Object.keys(c.cpu.registers).filter((register) => register != 'A')) {
+		    program[0] = opcode_lookup[reg].MVI;
+		    program[2] = opcode_lookup[reg].INR;
+		
+		    carry_set = c.cpu.flag_set(FlagType.Carry);
+		
+		    c.inject_program(program);
+		    c.execute_program();
+		
+		    // Here, we're just checking the carry flag hasn't been touched.
+		    assert.equal(c.cpu.flag_set(FlagType.Carry), carry_set);
+		
+		    assert.equal(c.cpu.registers[reg], 176);
+		    assert.equal(c.cpu.flag_set(FlagType.Parity),false);
+		    assert.equal(c.cpu.flag_set(FlagType.AuxillaryCarry), true);
+		    assert.equal(c.cpu.flag_set(FlagType.Zero), false);
+		    assert.equal(c.cpu.flag_set(FlagType.Sign), true);
+		
+		    assert.equal(c.cpu.clock, 19);
+		    
+		    c.reset();
+		};
+		});
+		
+});
