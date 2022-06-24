5e994ca85d4563dc523d36354dcbcfa6ff1ee0b5
Chris Akers <chris.j.akers@gmail.com>
2022-06-24 22:28:26 +0100

    Corrected RETURN unit tests

--- src/__tests__/return/rp.test.js
+++ src/__tests__/return/rp.test.js
@@ -8,19 +8,43 @@ describe('RP', () => {
 		const FlagType = c.cpu._flag_manager.FlagType;
 		
 		let program = [
+		
+		  // Store configurable value into the Accumulator
+		
 		  0x3E,                   // MVI into accumulator
 		  100,          // ...this byte
+		
+		  // Set stack pointer at address 0xFFFE
+		
 		  0x31,                   // Load stack pointer with...
 		  0xFE,                   // ..This low-byte
 		  0xFF,                   // ...and this high-byte
+		
+		  //  Move 0xAA into Register B and 0xAA into Register C,
+		  //  then push onto the Stack. (0xAAAA is the return address we will RET to)
+		
+		  0x06,                   // MVI into register B...
+		  0xAA,                   // ...This high addr byte
+		  0x0E,                   // MVI into register C...
+		  0xAA,                   // ...This low addr byte
+		  0xC5,                   // PUSH B
+		
+		  //  Move OpCode 0x76 (HALT) into memory location 0xAAAA (so the test will HALT when it RETS)
+		
 		  0x26,                   // MVI into Register H...
-		  0xFF,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x2E,                   // MVI into Register L...
-		  0xFE,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x36,                   // MVI into memory location (stored in registers H/L)
 		  0x76,                   // ...OpCode 0x76 (So the program HALTS when the program counter changes on JNZ)
+		
+		  // Now perform ADD
+		
 		  0xC6,                   // Add...
 		  87,                  // ...This immediate value to accumulator
+		
+		  // And check
+		
 		  0xF0,                   // RP
 		  0x76,                   // HALT
 		]
@@ -30,8 +54,8 @@ describe('RP', () => {
 		
 		  assert.equal(c.cpu.registers['A'], 187)
 		  assert.equal(c.cpu._flag_manager.IsSet(FlagType.Sign), true);
-		  assert.equal(c.cpu.program_counter, 15);
-		  assert.equal(c.cpu.Clock, 60);
+		  assert.equal(c.cpu.program_counter, 20);
+		  assert.equal(c.cpu.Clock, 85);
 		
 		});
 		
@@ -40,19 +64,43 @@ describe('RP', () => {
 		const FlagType = c.cpu._flag_manager.FlagType;
 		
 		let program = [
+		
+		  // Store configurable value into the Accumulator
+		
 		  0x3E,                   // MVI into accumulator
 		  10,          // ...this byte
+		
+		  // Set stack pointer at address 0xFFFE
+		
 		  0x31,                   // Load stack pointer with...
 		  0xFE,                   // ..This low-byte
 		  0xFF,                   // ...and this high-byte
+		
+		  //  Move 0xAA into Register B and 0xAA into Register C,
+		  //  then push onto the Stack. (0xAAAA is the return address we will RET to)
+		
+		  0x06,                   // MVI into register B...
+		  0xAA,                   // ...This high addr byte
+		  0x0E,                   // MVI into register C...
+		  0xAA,                   // ...This low addr byte
+		  0xC5,                   // PUSH B
+		
+		  //  Move OpCode 0x76 (HALT) into memory location 0xAAAA (so the test will HALT when it RETS)
+		
 		  0x26,                   // MVI into Register H...
-		  0xFF,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x2E,                   // MVI into Register L...
-		  0xFE,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x36,                   // MVI into memory location (stored in registers H/L)
 		  0x76,                   // ...OpCode 0x76 (So the program HALTS when the program counter changes on JNZ)
+		
+		  // Now perform ADD
+		
 		  0xC6,                   // Add...
 		  10,                  // ...This immediate value to accumulator
+		
+		  // And check
+		
 		  0xF0,                   // RP
 		  0x76,                   // HALT
 		]
@@ -62,8 +110,8 @@ describe('RP', () => {
 		
 		  assert.equal(c.cpu.registers['A'], 20)
 		  assert.equal(c.cpu._flag_manager.IsSet(FlagType.Sign), false);
-		  assert.equal(c.cpu.program_counter, 65535);
-		  assert.equal(c.cpu.Clock, 66);
+		  assert.equal(c.cpu.program_counter, 43691);
+		  assert.equal(c.cpu.Clock, 91);
 		
 		});
 		
--- src/__tests__/return/rpe.test.js
+++ src/__tests__/return/rpe.test.js
@@ -8,30 +8,53 @@ describe('RPE', () => {
 		const FlagType = c.cpu._flag_manager.FlagType;
 		
 		let program = [
+		
+		  // Store configurable value into the Accumulator
+		
 		  0x3E,                   // MVI into accumulator
 		  10,          // ...this byte
+		
+		  // Set stack pointer at address 0xFFFE
+		
 		  0x31,                   // Load stack pointer with...
 		  0xFE,                   // ..This low-byte
 		  0xFF,                   // ...and this high-byte
+		
+		  //  Move 0xAA into Register B and 0xAA into Register C,
+		  //  then push onto the Stack. (0xAAAA is the return address we will RET to)
+		
+		  0x06,                   // MVI into register B...
+		  0xAA,                   // ...This high addr byte
+		  0x0E,                   // MVI into register C...
+		  0xAA,                   // ...This low addr byte
+		  0xC5,                   // PUSH B
+		
+		  //  Move OpCode 0x76 (HALT) into memory location 0xAAAA (so the test will HALT when it RETS)
+		
 		  0x26,                   // MVI into Register H...
-		  0xFF,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x2E,                   // MVI into Register L...
-		  0xFE,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x36,                   // MVI into memory location (stored in registers H/L)
 		  0x76,                   // ...OpCode 0x76 (So the program HALTS when the program counter changes on JNZ)
+		
+		  // Now perform ADD
+		
 		  0xC6,                   // Add...
 		  10,                  // ...This immediate value to accumulator
+		
+		  // And check
+		
 		  0xE8,                   // RPE
 		  0x76,                   // HALT
 		]
-		
 		  c.InjectProgram(program);
 		  c.ExecuteProgram();
 		
 		  assert.equal(c.cpu.registers['A'], 20)
 		  assert.equal(c.cpu._flag_manager.IsSet(FlagType.Parity), true);
-		  assert.equal(c.cpu.program_counter, 65535);
-		  assert.equal(c.cpu.Clock, 66);
+		  assert.equal(c.cpu.program_counter, 43691);
+		  assert.equal(c.cpu.Clock, 91);
 		
 		});
 		
@@ -40,30 +63,53 @@ describe('RPE', () => {
 		const FlagType = c.cpu._flag_manager.FlagType;
 		
 		let program = [
+		
+		  // Store configurable value into the Accumulator
+		
 		  0x3E,                   // MVI into accumulator
 		  10,          // ...this byte
+		
+		  // Set stack pointer at address 0xFFFE
+		
 		  0x31,                   // Load stack pointer with...
 		  0xFE,                   // ..This low-byte
 		  0xFF,                   // ...and this high-byte
+		
+		  //  Move 0xAA into Register B and 0xAA into Register C,
+		  //  then push onto the Stack. (0xAAAA is the return address we will RET to)
+		
+		  0x06,                   // MVI into register B...
+		  0xAA,                   // ...This high addr byte
+		  0x0E,                   // MVI into register C...
+		  0xAA,                   // ...This low addr byte
+		  0xC5,                   // PUSH B
+		
+		  //  Move OpCode 0x76 (HALT) into memory location 0xAAAA (so the test will HALT when it RETS)
+		
 		  0x26,                   // MVI into Register H...
-		  0xFF,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x2E,                   // MVI into Register L...
-		  0xFE,                   // ...This high-byte
+		  0xAA,                   // ...This high-byte
 		  0x36,                   // MVI into memory location (stored in registers H/L)
 		  0x76,                   // ...OpCode 0x76 (So the program HALTS when the program counter changes on JNZ)
+		
+		  // Now perform ADD
+		
 		  0xC6,                   // Add...
 		  9,                  // ...This immediate value to accumulator
+		
+		  // And check
+		
 		  0xE8,                   // RPE
 		  0x76,                   // HALT
 		]
-		
 		  c.InjectProgram(program);
 		  c.ExecuteProgram();
 		
 		  assert.equal(c.cpu.registers['A'], 19)
 		  assert.equal(c.cpu._flag_manager.IsSet(FlagType.Parity), false);
-		  assert.equal(c.cpu.program_counter, 15);
-		  assert.equal(c.cpu.Clock, 60);
+		  assert.equal(c.cpu.program_counter, 20);
+		  assert.equal(c.cpu.Clock, 85);
 		
 		});
 		
