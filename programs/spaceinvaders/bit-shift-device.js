598cba7b6ae996cdab67b86fef486b355ba52744
Chris Akers <chris.j.akers@gmail.com>
2022-07-04 23:21:09 +0100

    Added BitShiftDevice for invaders

--- programs/spaceinvaders/bit-shift-device.js
+++ programs/spaceinvaders/bit-shift-device.js
@@ -7,15 +7,18 @@ import { Device } from './device.js';
 class BitShiftDevice extends Device {
 
     constructor() {
-        super();
-        this._register = 0x0;
-        this._bitShift = 0x0;
+        this._register = 0x0000;
+        this._bitShift = 0;
+    }
+
+    _dbgGetRegisterStr() {
+        return `BitShiftRegister: ${this._register}, 0x${this._register.toString(16).padStart(2,'0')}, ${this._register.toString(2).padStart(8,'0')}`;
     }
 
     Write(port ,val) {
         switch(port) {
             case 0x04:
-                this._register = val;
+                this._register = (val << 8) | (this._register >> 8 & 0xFF);
                 break;
             case 0x02:
                 this._bitShift = val;
@@ -26,7 +29,7 @@ class BitShiftDevice extends Device {
     Read(port) {
         switch(port) {
             case 0x03:
-                return this._register << this._bitShift;
+                return ((this._register << this._bitShift) >> 8) & 0xFF;
         }
     }
 }
