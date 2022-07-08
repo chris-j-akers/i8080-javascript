e3522d62be4a7120a0fa55f0a1f6be516ff3451b
Chris Akers <chris.j.akers@gmail.com>
2022-07-08 17:21:08 +0100

    BitShifter comments

--- programs/spaceinvaders/bit-shift-device.js
+++ programs/spaceinvaders/bit-shift-device.js
@@ -1,9 +1,13 @@
 import { Device } from './device.js';
 
-// For OUT, writing to port 2 sets the shift amount, and writing port 4 sets the
-// data in the shift registers. Reading via IN 3 returns the data shifted by the
-// shift amount. An implementation for my machine is something like this:
 
+/**
+ * The SpaceInvaders arcade machine included a specific bit of hardware - a
+ * bit-shifter - that enabled it to shift more than one bit at a time (all
+ * sprites were positioned using this). The 8080 itself only had the ability to
+ * shift one bit at a time which would have been too cumbersome. The BitShifter
+ * was just a device accessed by the IN and OUT instructions.
+ */
 class BitShiftDevice extends Device {
 
     constructor() {
