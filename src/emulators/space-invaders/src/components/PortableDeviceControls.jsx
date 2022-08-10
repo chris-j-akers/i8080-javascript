c022004f709afda2050794c577a4b6326f31b139
Chris Akers <chris.j.akers@gmail.com>
2022-08-10 22:56:45 +0100

    Changed to ontouch from onclick

--- src/emulators/space-invaders/src/components/PortableDeviceControls.jsx
+++ src/emulators/space-invaders/src/components/PortableDeviceControls.jsx
@@ -8,15 +8,21 @@ function PortableDeviceControls( {invadersWebWorker} ) {
   return (
     <div id='portable-device-controls' className='shadow'>
         <div id='direction-controls' className='shadow'>
-            <Button className='button-controls' icon={<AiFillCaretLeft />} onClick={() => {
-                invadersWebWorker.postMessage({Type: 'P1-LEFT-DOWN'});
-                setTimeout(() => invadersWebWorker.postMessage({Type: 'P1-LEFT-UP'}), 100);
-            }}>
+            <Button className='button-controls' icon={<AiFillCaretLeft />} 
+                ontouchstart={() => {
+                    invadersWebWorker.postMessage({Type: 'P1-LEFT-DOWN'});
+                }}
+                ontouchend={() => {
+                    invadersWebWorker.postMessage({Type: 'P1-LEFT-UP'})
+                }}>
             </Button>
-            <Button className='button-controls' icon={<AiFillCaretRight className='icons' />} onClick={() => {
-                invadersWebWorker.postMessage({Type: 'P1-RIGHT-DOWN'});
-                setTimeout(() => invadersWebWorker.postMessage({Type: 'P1-RIGHT-UP'}), 100);
-            }}>
+            <Button className='button-controls' icon={<AiFillCaretRight className='icons' />} 
+                ontouchstart={() => {
+                    invadersWebWorker.postMessage({Type: 'P1-RIGHT-DOWN'});
+                }}
+                ontouchend={() => {
+                    invadersWebWorker.postMessage({Type: 'P1-RIGHT-UP'});
+                }}>
             </Button>
         </div>
         <div id='fire-start-controls' className='shadow'>
@@ -24,15 +30,21 @@ function PortableDeviceControls( {invadersWebWorker} ) {
                 invadersWebWorker.postMessage({Type: 'COIN'});
             }}>
             </Button>
-            <Button icon={<FaRunning />} className='button-controls' onClick={() => {
-                invadersWebWorker.postMessage({Type: 'P1-START-DOWN'});
-                setTimeout(() => invadersWebWorker.postMessage({Type: 'P1-START-UP'}), 100);
-            }}>
+            <Button icon={<FaRunning />} className='button-controls' 
+                ontouchstart={() => {
+                    invadersWebWorker.postMessage({Type: 'P1-START-DOWN'});
+                }}
+                ontouchend={ () => {
+                    invadersWebWorker.postMessage({Type: 'P1-START-UP'});
+                }}>
             </Button>
-            <Button className='button-controls' icon={<GiBolterGun />} onClick={() => {
+            <Button className='button-controls' icon={<GiBolterGun />} 
+                ontouchstart={() => {
                 invadersWebWorker.postMessage({Type: 'P1-FIRE-DOWN'});
-                setTimeout(() => invadersWebWorker.postMessage({Type: 'P1-FIRE-UP'}), 100);
-            }}>
+                }} 
+                ontouchend={() => {
+                    invadersWebWorker.postMessage({Type: 'P1-FIRE-UP'});
+                }}>
             </Button>
         </div>
     </div>
