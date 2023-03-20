9121c39371ae6d27c34840095f859bc5a5af962b
cakers <chris.j.akers@gmail.com>
2023-03-20 12:24:21 +0000

    Changed LinkedIn URL

--- src/emulators/space-invaders/src/components/Header.jsx
+++ src/emulators/space-invaders/src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       <div>
         <p>This page is running a JavaScript Intel 8080 Virtual Machine in your browser.</p> 
         <p>It has been loaded with the original arcade ROM of <i>Space Invaders</i>.</p>
-        <p>The emulator and front-end were written by <a href='http://www.linkedin.com/in/chris-akers-1403114'>Chris Akers</a>.</p>
+        <p>The emulator and front-end were written by <a href='https://www.linkedin.com/in/chris-j-akers/'>Chris Akers</a>.</p>
         <p>For source code and docs, including a short tutorial on using the components to build your own Intel 8080 Virtual Machine, visit: <a href='https://github.com/chris-j-akers/i8080-javascript'>https://github.com/chris-j-akers/i8080-javascript</a></p>
       </div>  
     </div>
